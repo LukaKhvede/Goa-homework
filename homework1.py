a0a14d190f78490ad89472486c4181c1db6b945f
LukaKhvede <lukacho2604@gmail.com>
2026-02-03 23:08:50 +0400

    second commit

---
+++ homework1.py
@@ -0,0 +1,28 @@
+#davaleba 1 gamovitano print()it informacia pythonze
+print("Python is a popular, high-level, general-purpose programming language known for its clear, readable syntax and versatility. It is used in a wide array of applications, including web development, data science, artificial intelligence, and automation")
+#davaleba 2
+#print() aris brdzaneba romelsac vushvebt kompiutrshi
+#chven vutitebt print()-shi informacias romlis gamotanac gvsurs terminalshi
+#informacia sheidzleba iyos string tipi da mashin viyenebt("")brwyalebs
+#an sxva tipis informacia rogoric aris inegeri da float(am ors brwyalebi arunda)
+#printshi shegvidzlia aseve integerebis jami gamovtvalot da gamovitanot magalitad print(4 + 5)
+
+#davaleba 3
+#shegvidzlia orivestvis calke print davwerot
+chemi = 15
+megobris = 16
+print(chemi)
+print(megobris)
+
+print(chemi,"\n",megobris)
+#an gamovyot \n it ogond brwyalebshi
+#radgan roca komputeri xedavs \n-s cursori gadayavs axal xazze
+
+#davaleba 4
+
+ch="GOA"
+cj="is the "
+ck="best"
+print(ch+cj+ck)
+#gamoitana samive mimdevrobit rogorc xelit chavwere
+print('ss'+"dd")
---
+++ level01/classwork1.py
@@ -0,0 +1,5 @@
+name = "luka"#saxeli stringi
+surname = "khvedelidze"#gvari stringi
+age= 15 #asaki integeri
+height = 185 #simagle integeri
+print(name,surname,age,height)
