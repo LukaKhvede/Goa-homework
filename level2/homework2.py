c8014b2b548ec36121ad766f6dcb2d5416c9bd29
LukaKhvede <lukacho2604@gmail.com>
2026-02-07 22:40:11 +0400

    second commit

---
+++ level2/homework2.py
@@ -0,0 +1,54 @@
+#brwyalebshi chawerili ricxvsa da integers shoris gansxvaveba
+#roca ricxvs vwert brwyalebshi mas vashorebt integer tipis tituls da 
+#kompiuteri mas xedavs rogorc string tipis monacemad
+#es araris erori gamova terminalshi magram ar shegvidzlia gamoviyenot ise rogorc integeri
+
+#davaleba2
+num1=19
+num2=11
+Addition=num1+num2
+Substraction=num1-num2
+Multiplication=num1*num2
+Division=num1/num2
+print(Addition,"\n",Substraction,"\n",Multiplication,"\n",Division)
+#"\n"davuwere radgan mindoda 4ive sxvadasxva xazze gamoetana
+
+#davaleba3
+
+a=57
+b=4
+jami=a+b
+sxvaoba=a-b
+gayofa=a/b
+mtelricxvianigayofa=a//b
+nashti=a%b
+xarisxi=a**b
+print(jami,"jami","\n",sxvaoba,"sxvaoba","\n",gayofa,"gayofa","\n",mtelricxvianigayofa,"mtelricxvianigayofa","\n",nashti,"nashti","\n",xarisxi,"xarisxi")
+#aqac isev mindoda gamometana sxvadasxva xazze da shemdeg mivanishne ra ra aris
+
+#davaleba4
+
+x = 10
+x = x + 5
+x = x * 2
+print(x)
+#x-ma gamoitana 30 radgan imis shemdeg rac chven x-s mivaniwet 10is toli mnishvneloba
+#qvemot davuweret rom X udrida wina X-s damatebuli 5 anu 15s aq moxda overwrite radgan
+#pythonshi kodi zemodan qvemot ikitxeba kompiuterma jer sheinaxa x-is mnishvneloba 10
+#shemdeg gadaaketa 15 ad shemdeg ki isev gadaaketa 30-ad radgan qvemot davuweret rom
+#x udrida wina x gamravlebuli 2-ze amitom kompiuterma 15 gaamravla 2-ze da 30 
+#sheinaxa x-is mnishvnelobad amitomac gamoitana print(x)-ma 30
+
+#davaleba 5
+
+balance=100
+balance=balance-30
+balance=balance+50
+balance=balance*2
+print(balance)
+#overwrite is daxmarebit balance gaxda 240
+
+#davaleba 6
+
+group="Group 96 is the best"
+print(group*96)
