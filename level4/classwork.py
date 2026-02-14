9549864bf99ed3dd77de5e79366e94e56d2b82de
LukaKhvede <lukacho2604@gmail.com>
2026-02-14 17:33:39 +0400

    second commit

---
+++ level4/classwork.py
@@ -0,0 +1,17 @@
+user1 = "Nika"
+user2 = "Lika"
+print(user2)
+#cvladi ver daiwyeba cifrit an qvedatireti
+#aseve stringebi unda iyos dawerili ertnairi brwyalebit 
+#aseve printshi sworad unda chavwerot cvladis saxeli ertnairi zomis asoebit
+
+name=input("enter your name: ")
+surname=input("enter your surname: ")
+adress=input("enter your addres")
+age=input("enter your age: ")
+print(f'I am {name} {surname}, I live in {adress} and I am {age} years old')
+
+
+email=input("enter your email: ")
+password=input("enter your password: ")
+print(email, "\n" , password)
---
+++ level4/homework.py
@@ -0,0 +1,64 @@
+#davaleba 1
+#cudi praqtikaa roca cvlad vaniwebt iset saxels rogori funqciac arsebobs magalitad: sum,print
+#aseve cvladis win ar unda davwerot raime ricxvi magalitad: 1num 
+#ar unda davwerot space cvladis saxelshi magalitad: age and height
+
+#davaleba 2
+# debbuging ewodeba roca chven vasworebt arasworad daweril kods 
+# magalitad : 1num=9
+# print(i am 1num years old)
+# cvladi arasworadaa chawerili 1num is magivrad num1 unda eweros
+#aseve printshi gavasworot print(f"i am {num1} years old")aq gamoviyene f stringi 
+# magram undoda ubralod brwyalebi num1 is garda
+
+#davaleba 3
+#TypeError: Raised when an operation is performed on an inappropriate or incompatible data type, such as adding a string to an integer.
+#anu roca integer tips an float tips vsvavt brwyalebshi programa mas stringad agiqvavs
+
+#NameError: Occurs when a variable or function name is used but has not been defined.
+#es errori gvxvdeba roca  vcdilobt gamoviyenot cvladi romelic aqamde ar shegviqmnia
+
+#davaleba 4
+
+name = "Lika"
+print(name)
+
+number = "5"
+text = " apples"
+resault=number+text
+print(resault)
+
+name = "liKa"
+name2 = name + "4"
+
+firstuser = "Lika"
+seconduser = "Giorgi"
+print(firstuser)
+
+firstname = "Data"
+lastname = "random"
+
+first_user = "Lika"
+nduser = "Giorgi"
+print(first_user)
+
+#davaleba 5
+
+city=input("enter your city: ")
+district=input("enter your district: ")
+building=input("enter your building: ")
+floor=input("enter your floor: ")
+print(f"I live in {city} on {district} in {building} on {floor} floor")
+
+#davaleba6
+age=input("enter your age: ")
+print(f"You are {age}g years old")
+
+#davaleba7
+num1=input("enter any number: ")
+num2=input("enter second number: ")
+sum=num1+num2
+#
+multiply=num1*num2
+subtraction=num1-num2
+print(sum,division,multiply,subtraction)
