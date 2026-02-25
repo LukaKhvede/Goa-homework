ef110c9f155816fb97af7d41289f0787224744e1
LukaKhvede <lukacho2604@gmail.com>
2026-02-25 23:55:54 +0400

    second commit

---
+++ level6/classwork(level6).py
@@ -0,0 +1,29 @@
+num1=int(input("enter your number: "))
+num2=int(input("enter your number: "))
+g1=num1+num2
+g2=num1-num2
+g3=num1*num2
+g4=num1/num2
+g5=num1%num2
+g5=num1//num2
+g6=num1**num2
+print(g1,g2,g3,g4,g5,g6)
+
+#davaleba 2
+
+print(bool(int(True) - int(False) + int(False)))
+#es kodi gamoitans trues radgan iqneba bool(1-0+0) 
+#rac aris bool(1) da aris true radgan vicit rom trues mnishvneloba 1 da false 0
+#xolo bools es ricxvi gadayavs isev true an false shi am shemtxvevashi gamova true
+
+
+#davaleba 3
+
+int()
+str()
+float()
+#es funqciebi aris data conversation anu tu gvaq int("99")
+#gamova 99 radgan stringi gadavida intshi
+#aseve str sheidzleba gadavides floatshi (tu gvaqvs ricxvebi da ara asoebi)
+#sheidzleba float gadavides int shi magalitad 12.5 gaxdeba 12 radgan amrgvalebs umciresisken
+
---
+++ level6/homework.py
@@ -0,0 +1,50 @@
+#type funqcia adgens ra tipis monacemi inaxeba cvladshi an tviton monacemi ratipis aris shemdeg gamoaqvs informacia tu ra tipis cvladi inaxeba
+#magalitad tu integere tipia gamova class<int> aseve gamoiyeneba boolianebtan
+num1=5
+print(type(num1))
+num2=3.5
+print(type(num2))
+name=input("enter your name: ")
+print(type(name))
+print(type(3.14))
+print(type(True))
+
+#davaleba 2
+
+age= 16
+gvari= "khvedelidze"
+saxeli= "luka"
+print("I am" + " " + saxeli + " " + gvari + " " + "and  i am " + str(age) + " " + "years old")
+
+
+#davaleba 3
+
+pirveli=float(input("enter your number: "))
+meore=float(input("enter your number: "))
+jami=pirveli+meore
+gamokleba=pirveli-meore
+gayofa=pirveli/meore
+gamravleba=pirveli*meore
+print(jami,gamokleba,gayofa,gamravleba)
+
+#davaleba 4
+
+number1=int(input("enter your number: "))
+number2=int(input("enter your number: "))
+number3=int(input("enter your number: "))
+number4=int(input("enter your number: "))
+number5=int(input("enter your number: "))
+sashualo=(number1+number2+number3+number4+number5)/5
+print(sashualo)
+
+#davaleba 5
+
+celsius=float(input("enter temperature in celsius: "))
+farenheit=	(celsius * 9/5) + 32
+print(f"{farenheit} farenheit")
+
+#davaleba 6
+
+parenheit=float(input("enter temperature in parenheit: "))
+celsiusi=(parenheit - 32) * (5/9)
+print(f"{celsiusi} celsiusi")
