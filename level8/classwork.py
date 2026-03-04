093fc946f85346393431eac636e27683a6566cbf
LukaKhvede <lukacho2604@gmail.com>
2026-03-04 19:02:11 +0400

    second commit

---
+++ level8/classwork.py
@@ -0,0 +1,23 @@
+#davaleba 1
+#True and True gamoitans Trues
+#True and False gamoitans False
+#False and False gamoitans False
+#False and Trues gamoitans False
+#True or False gamoitans Trues
+#True or True gamoitans Trues
+#False or True gamoitans Trues
+# False or False gamoitans False
+
+#davaleba 2
+#True and False or False or True and True and False or True
+#True and False jer gaxdeba False aseve True and True gaxdeba True
+#shemdeg dagvrcheba False or False or True or True False or False aris False
+#False or True gaxdeba True da True or True gaxdeba True
+#es kodi gamoitans Trues
+
+#davaleba3
+
+familymembers=int(input("members: "))
+thief_detected=familymembers>4
+print(thief_detected)
+
---
+++ level8/homework.py
@@ -0,0 +1,75 @@
+#davaleba1
+# > - metia
+# < - naklebia
+# >= - metia an tolia
+# <= - naklebia an tolia
+# == - udris (amowmebs udris tuara monacemi da monacemis tipi)
+# != - ar udris(amowmebs udris tuara monacemi da monacemis tipi)
+num1=9
+num2=10
+print(num2>num1)#gamova ture radgan num2 shi shenaxuli cifri metia
+num3=7
+num4=7
+num5="7"
+print((num1*num2)>num3)#aqac gamova true radgan 9 da 10is namravli metia 7ze
+print(num2<num1)#gamova false radgan 10 metia 9ze
+print((num2-num1)>num3)#gamova false radgan 7 metia 1ze
+print(num1>=num2)#gamova false radgan 9 arc metia da arc toli 10ze
+print(num2>=num1)#gamova true radgan 10 metia 9ze magram ar aris toli 9is
+print(num2<=num3)#gamova false radgan 10 arc naklebia da arc toli 7is
+print(num3<=num4)#gamova true radgan 7 tolia 7 is
+print(num3==num4)#gamova true radgan 7 udris 7s
+print(num4==num5)#aq gamoa false radgan miuxedavad imisa rom terminalshi ertnairad gamova erti string tipia meore integer tipi amitom false
+print(num4!=num3)#gamova false radgan 7i aris 7is toli
+print(num4!=num5)#gamova true radgan rogorc zemot vaxsene sxvadasxva tipisaa
+
+#davaleba 2
+#chven vicit 2 logikuri operatori and da or
+#and ufro mkacria radgan moitxovs rom yvela piroba sruldebodes
+#roca or ufro ketilia da mistvis mtavaria 1 piroba mainc sruldebodes
+#magalitad:
+print(True and False)#gamova false radgan roca gvaqvs and da 1 mainc false aris gamova false
+print(True or False)#gamova true radgan or roca gvaqvs 1 true sakmarisia
+
+#davaleba 3
+height=int(input("enter your height in cantimeters: "))
+myheight=187
+print(height>=myheight)#aq gavigebt chemze magalia dabali tu chemi simaghle
+
+#davaleba4
+
+num1 = "21"
+num2 = 21
+print(num1 == num2)
+#es kodi gamoitans false radgan rogorc zemot vaxsene chven sheidzleba terminalshi vxedavt rom orive 21 aris magram kompiuteri
+#amas sxva gvarad agiqvavs igi xvdeba rom es ori sxvadasxva tipisaa erti string tipis meore ki integeri
+#rogorc chven vicit == shedarebis operatori ara mxolod ori mnishvnelobis sidides amowmevs aramed ra tipis monacemic aris
+
+#davaleba5
+
+mysurname="Khvedelidze"
+surname=input("enter your surname: ")#aq ar vutiteb ro str s radgan inputis dros yoveltvis stingi shemogvaq
+print(mysurname==surname)
+
+#davaleba6
+False or True and True and False#->gamova false jer vasrulebt and-s shemdeg or-s
+True and False or False or True#->gamova true igive principit
+True or True and False or True or False and True or False#->gamova true esec igive principit ketdeba 
+
+#davaleba7
+
+temperatura=30
+temperatura1=float(input("enter temperature: "))
+print(temperatura1>temperatura)
+
+
+#davaleba 8
+farenheiti=89.6
+farenheiti1=float(input("enter temperature in degrees: "))
+farenheiti2=(farenheiti1*(9/5))+32
+print(farenheiti2>farenheiti)
+
+
+
+
+
