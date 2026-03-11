d7d6203e0cbb928650ca8dc8d6c70642f521b630
LukaKhvede <lukacho2604@gmail.com>
2026-03-11 21:28:58 +0400

    second commit

---
+++ level10/homework.py
@@ -0,0 +1,67 @@
+#davaleba2
+
+
+for i in range(50,200):
+    print(i)
+
+# #DAVALEBA3
+for i in range(0,100,2):
+    print(i)
+    
+#davaleba4
+
+for i in range(101,150,2):
+    print(i)
+
+# #davaleba 5
+
+num1=1
+while num1<=50:
+    print(num1)
+    num1+=1
+
+# #davaleba6
+
+num2=20
+while num2<=60:
+    print(num2)
+    num2+=5
+
+#davaleba7
+
+name=input("enter your name: ")
+for name in name:
+    print(name)
+
+#davaleba8
+
+raodenoba=300
+while raodenoba>0:
+    print(f"You have bought the drink{raodenoba}")
+    raodenoba-=2
+print("Out of stock")
+
+
+#davaleba9
+
+for i in range(50,20,-3):
+    print(i)
+
+# #davaleba10
+
+num4=150
+while num4>=0:
+    print(num4)
+    num4-=1
+
+#davaleba11
+
+#for loop gvexmareba martivad da swrafad gamovitanot raime mnishvneloba terminalshi imdenjer ramdenjerac gvinda
+#aseve for loopis daxmarebit shegvidzlia gamovitanot aso-aso dashlili winadadeba,gamovitanot ragac ricxvidan ragac ricxvamde
+#ise rom gamoviyenot matematikuri operatorebi magalitad akvadratdes an miematos 4
+
+
+#davaleba12
+#while loop aris for loopis msgavsi ogond aq gvwirdeba cvladi sadac shevinaxavt sawyis mnishvnelobas
+#while loop mushaobs iqamde sanam piroba romelsac chven vaniwebt aris true rogorc ki false gaxdeba cikli sruldeba
+#counter cvladi gvwirdeba imistvis rom cikli ar gacherdes ert adgilas da infinite loop ar moxdes aseve gvexmareba imashi rom anishnebs cikls ra momentshi gacherdes
