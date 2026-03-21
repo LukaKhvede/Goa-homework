4c1395b463beed889bc522185595bb9f822567c3
LukaKhvede <lukacho2604@gmail.com>
2026-03-22 01:04:25 +0400

    second commit

---
+++ level14/homework.py
@@ -0,0 +1,22 @@
+#davaleba1
+phrase = "Python Programming"
+print(phrase[0:6])
+print(phrase[7:])
+print(phrase[0::3])
+#davaleba2
+text = "PythonSlicing"
+print(text[0:6])
+print(text[::-1])
+print(text[::2])
+#davaleba3
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i']
+letterss=letters[::2]
+print(letterss)
+print(letters[2:7:2])
+print(letters[-5::-1])
+#davaleba4
+sentence = "Slicing makes Python powerful"
+print(sentence[14:20])
+print(sentence[:-9:-1])
+sentences=sentence[::3]
+print(sentences)
