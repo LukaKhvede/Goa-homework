62a0856fe4df74aeb1e634bb7899b0a478888bb4
LukaKhvede <lukacho2604@gmail.com>
2026-08-26 21:49:30 +0400

    second commit

---
+++ level55/script.js
@@ -0,0 +1,58 @@
+// let-ით შექმნილი ცვლადის მნიშვნელობის შეცვლა შეიძლება.
+// const-ით შექმნილი ცვლადის მნიშვნელობის შეცვლა არ შეიძლება.
+
+// let
+let age = 17;
+age = 18; // სწორია
+
+// const
+const country = "Georgia";
+country = "USA"; // Error
+
+
+
+
+//2
+
+let firstName = "Luka";
+let lastName = "Khvedelidze";
+let favoriteColor = "Black";
+let favoriteFood = "Pizza";
+let favoriteGame = "CS2";
+
+console.log(firstName);
+console.log(lastName);
+console.log(favoriteColor);
+console.log(favoriteFood);
+console.log(favoriteGame);
+
+// მნიშვნელობების შეცვლა
+firstName = "Giorgi";
+lastName = "Beridze";
+favoriteColor = "Blue";
+favoriteFood = "Burger";
+favoriteGame = "Minecraft";
+
+// თავიდან გამოტანა
+console.log(firstName);
+console.log(lastName);
+console.log(favoriteColor);
+console.log(favoriteFood);
+console.log(favoriteGame);
+
+
+
+//3
+
+const countryy = "Georgia";
+const capitalCity = "Tbilisi";
+const city = "Batumi";
+
+console.log(countryy, capitalCity, city);
+
+
+//4
+
+let myVariable;
+
+console.log(myVariable);
