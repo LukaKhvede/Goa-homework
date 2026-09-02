47c76adb040e5cbff5fa401b794e9fc85f868f35
LukaKhvede <lukacho2604@gmail.com>
2026-09-02 16:53:03 +0400

    second commit

---
+++ level57/script.js
@@ -0,0 +1,25 @@
+let word1="Hello";
+let word2="World";
+
+console.log(word1);
+console.log(word2);
+
+word1=word1.toUpperCase();
+word2=word2.toLowerCase();
+
+console.log(word1);
+console.log(word2);
+//2
+
+const variable='     Group 71      ';
+console.log(varuable.trim());
+
+//3
+
+//math.random()abrunebs shemtxvevit mnishvnelobas 0idan 1amde
+//math.floor()ricxvs amrgvalebs qvemot myof mtel ricxvamde
+
+//4
+
+let rundomNum = Math.floor(Math.random()*97)
+console.log(rundomNum)
